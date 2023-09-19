3922dba426c15de925698ac30670281427ee9d60
kke008 <130429793+kke008@users.noreply.github.com>
2023-09-19 11:51:55 -0700

    Update LetterCount.java

--- LetterCount.java
+++ LetterCount.java
@@ -6,9 +6,10 @@ import java.util.Scanner;
  *	@since	September 14, 2023
  */
 public class LetterCount {
-	private int[] count;
-	private int[] scaled;
 	// Fields go here, all must be private
+	private int[] count;	// stores the number of times a letter appears
+	private int[] scaled;	// stores the number of x's in the bars for
+							// each letter
 	
 	/* Constructor */							
 	public LetterCount() {
@@ -16,7 +17,7 @@ public class LetterCount {
 		scaled = new int[26];
 	}
 	
-	/* Main routine */
+	/** Main routine */
 	public static void main(String[] args) {
 		LetterCount lc = new LetterCount();
 		if (args.length != 1)
@@ -40,17 +41,21 @@ public class LetterCount {
 		while (read.hasNext()) {
 			line = read.nextLine();
 			for (int i = 0; i < line.length(); i++) {
-				int num = Integer.parseInt(line(i));
-				if (num > 96 && num < 123) {
-					charNum = num - 97;
+				String letter = line.substring(i, i + 1);
+				for (int m = (int)('a'); 
+				/*
+				int numOfChar = Integer.parseInt(line.substring[i, i+1]);
+				if (numOfChar >= ((int)('a') && numOfChar <= (int)('z') ) {
+					charNum = numOfChar - (int)('a');
 					count[charNum] += 1;
 				}
 				
-				else if (num > 64 && num < 91) {
-					charNum = num + 32;
+				else if (numOfChar >= (int)('A') && numOfChar < 91) {
+					charNum = numOfChar + 32;
 					count[charNum] += 1;
 				}
 			}
+			*/
 		}
 		read.close();
 		scaleCounts();
@@ -68,15 +73,18 @@ public class LetterCount {
 		}
 	}
 	
+	/** This method finds the frequency of the letter that appears most
+	 * 	and uses it to find a scale for the other frequencies, then scales
+	 * 	them. The results are stored in a new array.
+	 */
 	public void scaleCounts() {
-		int scale = 1;
 		int indexOfMost = 0;
 		
 		for (int j = 1; j < 26; j++) {
 			if (count[indexOfMost] < count[j])
 				indexOfMost = j;
 		}
-		scale = count[indexOfMost] / 60;
+		int scale = count[indexOfMost] / 60;
 		
 		for (int k = 0; k < 26; k++) {
 			scaled[k] = count[k] / scale;
