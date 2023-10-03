e872c2e17c1e2bc113837e55574aaa2a315c42e6
kke008 <130429793+kke008@users.noreply.github.com>
2023-10-03 11:39:19 -0700

    Update Yahtzee.java

--- Yahtzee.java
+++ Yahtzee.java
@@ -1,3 +1,6 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 /**
  *	Introduce the game here
  *
@@ -6,7 +9,30 @@
  */
  
 public class Yahtzee {
-
+	private final ROUNDS = 13;	// 13 rounds per game
+	private YahtzeePlayer player1;	// Player 1 info
+	private DiceGroup p1Dice;	// Player 1's dice
+	private YahtzeePlayer player2;	// Player 2 info
+	private DiceGroup p2Dice;	// Player 2's dice
+	
+	public Yahtzee() {
+		 player1 = new YahtzeePlayer();
+		 p1Dice = new DiceGroup();
+		 player2 = new YahtzeePlayer();
+		 p2Dice = new DiceGroup();
+	}
+	
+	public static void main(String [] args) {
+		Yahtzee yz = new Yahtzee();
+		yz.printHeader();
+		yz.getPlayerNames();
+		for (int played = 1; played <= ROUNDS; played++) {
+			System.out.printf("Round %d of %d rounds.\n\n", played, ROUNDS);
+			yz.playRound(player1, p1Dice);
+			yz.playRound(player2, p2Dice);
+		}
+		//yz.winner();
+	}
 	
 	public void printHeader() {
 		System.out.println("\n");
@@ -30,15 +56,86 @@ public class Yahtzee {
 		System.out.println("\n\n");
 	}
 	
+	/**	Gets the players names and prompts them to roll the dice to determine
+	 * 	who will go first. The player who goes first will be player 1 and
+	 * 	the player who goes second will be player 2.
+	 */
 	public void getPlayerNames() {
-		String name1 = "";	// name of player 1
-		name1 = Prompt.getString("Player 1, please enter your first name ");
-		YahtzeePlayer player1 = new YahtzeePlayer();
-		player1.setName(name1);
+		String p1Name = "";	// name of player who goes first (temporarily 
+							// player who gives name first)
+		p1Name = Prompt.getString("Player 1, please enter your first name ");
 		
-		String name2 = "";	// name of player 2
-		name2 = Prompt.getString("Player 2, please enter your first name ");
-		YahtzeePlayer player2 = new YahtzeePlayer();
-		player2.setName(name2);
+		String p2Name = "";	// name of player who goes second (temporarily 
+							// player who gives name second)
+		p2Name = Prompt.getString("\n\nPlayer 2, please enter your first name ");
+		
+		int p1Total = 0;
+		int p2Total = 0;
+		while (p1Total == p2Total) {
+			System.out.printf("\n\nLet's see who will go first. %s please hit " +
+				"enter to roll the dice :", p1Name);
+			p1Dice.rollDice();
+			p1Dice.printDice();
+			p1Total = p1Dice.getTotal();
+			
+			System.out.printf("\n\n%s, it's your turn. Please hit enter to roll" +
+				" the dice :", p2Name);
+			p2Dice.rollDice();
+			p2Dice.printDice();
+			p2Total = p2Dice.getTotal();
+			
+			if (p1Total == p2Total)
+				System.out.printf("\n\nWhoops, we have a tie (both rolled " +
+					"%d. Looks like we'll have to try that again...", p1Total);
+		}
+		
+		System.out.printf("\n\n%s, you rolled a sum of %d, and %s, you " +
+			"tolled a sum of %d.", p1Name, p1Total, p2Name, p2Total);
+		
+		if (p1Total < p2Total) {
+			String tempName = p1Name;
+			p1Name = p2Name;
+			p2Name = tempName;
+		}
+		
+		System.out.printf("\n%s, since your sum was higher, you'll " +
+				"roll first.\n\n", p1Name);
+		
+		player1.setName(p1Name);
+		player2.setName(p2Name);
+		
+	}
+	/*
+	class PlayerListener implements KeyListener {
+		public void keyPressed(KeyEvent evt) {
+			int code = evt.getKeyCode();
+			if (code == VK_ENTER)
+		}
+	}
+	*/
+	
+	/**	Method where each round of the game (13 total) is played. The
+	 * 	player rolls their dice a max of 3 times and chooses which dice 
+	 *  to hold each time, then they choose which category to put their 
+	 *  score in. 
+	 */
+	public void playRound(YahtzeePlayer player, DiceGroup playerDice) {
+			System.out.printf("%s, it's your turn to play. Please hit " +
+				"enter to roll the dice :\n\n", player.getName());
+			playerDice.rollDice();
+			playerDice.printDice();
+			String hold = "";
+			hold = Prompt.getString("Which di(c)e would you like to keep? " +
+				"Enter the values you'd like to 'hold' without spaces. " +
+				"For examples, if you'd like to 'hold' die 1, 2, and 5, " +
+				"enter 125\n(enter -1 if you'd like to end the turn)\n");
+			/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		}
+	}
+	
+	/** Totals players' scores to figure out who won, and tells them.
+	 */
+	public void winner() {
+		
 	}
 }
