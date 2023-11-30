c06bb51e78019b3d78171e4e99518c85fdddf05b
kke008 <130429793+kke008@users.noreply.github.com>
2023-11-30 11:36:17 -0800

    Add files via upload

---
+++ SortMethods.java
@@ -0,0 +1,135 @@
+/**
+ *	SortMethods - Sorts an array of Integers in ascending order.
+ *
+ *	@author Mr.Greenstein and Karen Ke
+ *	@since	November 30, 2023
+ */
+public class SortMethods {
+	
+	/**
+	 *	Bubble Sort algorithm - in ascending order
+	 *	@param arr		array of Integer objects to sort
+	 */
+	public void bubbleSort(Integer [] arr) {
+		for (int outer = arr.length - 1; outer > 0; outer--)
+			for (int inner = 0; inner < outer; inner++)
+				if (arr[inner].compareTo(arr[inner + 1]) > 0)
+					swap(arr, inner, inner + 1);
+	}
+	
+	/**
+	 *	Swaps two Integer objects in array arr
+	 *	@param arr		array of Integer objects
+	 *	@param x		index of first object to swap
+	 *	@param y		index of second object to swap
+	 */
+	private void swap(Integer[] arr, int x, int y) {
+		Integer temp = arr[x];
+		arr[x] = arr[y];
+		arr[y] = temp;
+	}
+	
+	/**
+	 *	Selection Sort algorithm - in ascending order (you implement)
+	 *	@param arr		array of Integer objects to sort
+	 */
+	public void selectionSort(Integer [] arr) {	/////////////////////////////////////// DOESN"T WORK
+		for (int outer = arr.length - 1; outer > 0; outer--) {
+			int tempMaxIndex = 0;
+			for (int inner = 0; inner < outer; inner++) {
+				if (arr[inner].compareTo(arr[tempMaxIndex]) > 0)
+					System.out.println (inner + ": " + arr[inner] + "\t\t" + tempMaxIndex + "; " + arr[tempMaxIndex]);
+				
+				System.out.println (arr[tempMaxIndex] + "\n\n");
+			}
+			swap(arr, tempMaxIndex, outer);	
+		}
+	}
+	
+	/**
+	 *	Insertion Sort algorithm - in ascending order (you implement)
+	 *	@param arr		array of Integer objects to sort
+	 */
+	public void insertionSort(Integer [] arr) {}
+	
+	/**
+	 *	Merge Sort algorithm - in ascending order (you implement)
+	 *	@param arr		array of Integer objects to sort
+	 */
+	public void mergeSort(Integer [] arr) {}
+	
+	/*****************************************************************/
+	/************************* For Testing ***************************/
+	/*****************************************************************/
+	
+	/**
+	 *	Print an array of Integers to the screen
+	 *	@param arr		the array of Integers
+	 */
+	public void printArray(Integer[] arr) {
+		if (arr.length == 0) System.out.print("(");
+		else System.out.printf("( %4d", arr[0]);
+		for (int a = 1; a < arr.length; a++) {
+			if (a % 10 == 0) System.out.printf(",\n  %4d", arr[a]);
+			else System.out.printf(", %4d", arr[a]);
+		}
+		System.out.println(" )");
+	}
+
+	public static void main(String[] args) {
+		SortMethods se = new SortMethods();
+		se.run();
+	}
+	
+	public void run() {
+		Integer[] arr = new Integer[10];
+		// Fill arr with random numbers
+/*		for (int a = 0; a < 10; a++)
+			arr[a] = (int)(Math.random() * 100) + 1;
+		System.out.println("\nBubble Sort");
+		System.out.println("Array before sort:");
+		printArray(arr);
+		System.out.println();
+		bubbleSort(arr);
+		System.out.println("Array after sort:");
+		printArray(arr);
+		System.out.println();
+/*/		
+		
+		for (int a = 0; a < 10; a++)
+			arr[a] = (int)(Math.random() * 100) + 1;
+		System.out.println("\nSelection Sort");
+		System.out.println("Array before sort:");
+		printArray(arr);
+		System.out.println();
+		selectionSort(arr);
+		System.out.println("Array after sort:");
+		printArray(arr);
+		System.out.println();
+		
+/*		
+		for (int a = 0; a < 10; a++)
+			arr[a] = (int)(Math.random() * 100) + 1;
+		System.out.println("\nInsertion Sort");
+		System.out.println("Array before sort:");
+		printArray(arr);
+		System.out.println();
+		insertionSort(arr);
+		System.out.println("Array after sort:");
+		printArray(arr);
+		System.out.println();
+*/
+/*		
+		for (int a = 0; a < 10; a++)
+			arr[a] = (int)(Math.random() * 100) + 1;
+		System.out.println("\nMerge Sort");
+		System.out.println("Array before sort:");
+		printArray(arr);
+		System.out.println();
+		mergeSort(arr);
+		System.out.println("Array after sort:");
+		printArray(arr);
+		System.out.println();
+*/
+	}
+}
