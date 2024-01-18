182412244796150615522eb073e71dd2cdf6a928
kke008 <130429793+kke008@users.noreply.github.com>
2024-01-18 11:34:52 -0800

    Update SudokuMaker.java

--- SudokuMaker.java
+++ SudokuMaker.java
@@ -44,22 +44,11 @@ public class SudokuMaker {
 	 *    - if all 9 values fail, createPuzzle is called for the previous
 	 * 		grid location and a different random value is assigned
 	 * 
-	 *  @param row		the row of the current value in the grid
-	 *  @param column	the column of the current value in the grid
+	 *  @param r		the row of the current value in the grid
+	 *  @param c	the column of the current value in the grid
 	 *  @param valWorks	whether or not the value works
 	 */
-	public boolean createPuzzle(int row, int column) {
-		boolean valWorks = false;
-		int val = (int)(Math.random()*9 + 1);
-		
-		//while (valWorks == false) {
-		//	if (puzzle[row] == 
-		//}
-		
-		return valWorks;
-	}
-	
-	public int getVal(int r, int c) {
+	public boolean createPuzzle(int r, int c) {
 		int[] nums = new int[9];
 		for (int i = 0; i < 9; i++) {
 			int num = (int)(Math.random()*9 + 1);
@@ -76,9 +65,31 @@ public class SudokuMaker {
 			}
 		}
 		
-		int val = -1;
-		for (int k = 0; k < 9; k++) {
-			if (
+		boolean valWorks = true;
+		int k;
+		for (k = 0; k < 9; k++) {	// checking all possible values of val
+			int val = nums[k];
+			for (int l = 0; l < puzzle[r].length(); l++) {	// checking row
+				if (val == puzzle[r][l])
+					valWorks = false;
+			}
+			
+			for (int m = 0; m < puzzle.length; m++) {	// checking column
+				if (val == puzzle[m][c])
+					valWorks = false;
+			}
+		}
+		
+		if (k == 9) {
+			// if valWorks and not last row or not last column
+			if (valWorks && (r < puzzle.length - 1 || c < puzzle[r].length - 1) {
+				if (c >= puzzle[r].length - 1)	// if last column:
+					createPuzzle(r + 1, 0);
+				else 	// if last row
+					createPuzzle(r, c + 1);
+			}
+			
+			else/////////////////////////////////////////////////////////////////////////////////////////////////
 		}
 	}
 	
