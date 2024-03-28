fc0b0ab5533b320497b04748e2cd5566a3fbdf5b
kke008 <130429793+kke008@users.noreply.github.com>
2024-03-27 22:09:46 -0700

    Create Egg.java

---
+++ Egg.java
@@ -0,0 +1,5 @@
+/* Create an Egg class that extends the Actor class. An Egg never moves. It starts as
+Color.WHITE and darkens in color with each step. When the number of steps reaches
+45, the Egg turns Color.RED. When the Egg reaches 50 steps, it is replaced by a new
+Chicken
+/
