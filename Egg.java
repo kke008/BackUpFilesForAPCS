4a0f6a8c728542c86d12b8b7b55c9b3767943c1b
kke008 <130429793+kke008@users.noreply.github.com>
2024-03-28 11:46:42 -0700

    Update Egg.java

--- Egg.java
+++ Egg.java
@@ -4,41 +4,40 @@
  * Chicken
 */
 
-import java.info.gridworld.actor.Actor;
-import java.info.gridworld.grid.Grid;
-import java.info.gridworld.grid.Location;
-import java.util.Color;  //////////////////////////////////
+import info.gridworld.actor.Actor;
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+import java.awt.Color;
+
 public class Egg extends Actor {
-  private int steps;
-  private static final DARKENING_FACTOR = 0.05;
-  public Egg() {
-    steps = 0;
-    setColor(Color.WHITE);
-  }
+	private int steps;
+	private static final double DARKENING_FACTOR = 0.05;
+	public Egg() {
+		steps = 0;
+		setColor(Color.WHITE);
+	}
 
-  /* Overrides act() in Actor. Decrement steps. If steps has reached 45,
-   * sets it to Color.RED. If steps has reached 50, replaces the Egg with
-   * a Chicken. Darkens color otherwise.
-   */
-  public void act() {
-    steps++;
-    if (steps == 45) 
-      setColor(Color.RED);
-    else if (steps == 50) {
-      Chicken chick = new Chicken();
-      getGrid().put(getLocation(), chick);
-    }
-    else {
-      Color c = getColor();
-      int red = c.getRed() * (1 - DARKENING_FACTOR);
-      int green = c.getGreen() * (1 - DARKENING_FACTOR);
-      int blue = c.getBlue() * (1 - DARKENING_FACTOR);
-
-      Color newC = new Color();
-      newC.setRed(red);
-      newC.setGreen(green);
-      newC.setBlue(blue);
-      setColor(newC);
-    }
-  }
+    /* Overrides act() in Actor. Decrement steps. If steps has reached 45,
+     * sets it to Color.RED. If steps has reached 50, replaces the Egg with
+     * a Chicken. Darkens color otherwise.
+     */
+    public void act() {
+		steps++;
+		if (steps == 45) 
+			setColor(Color.RED);
+			
+		else if (steps == 50) {
+			Chicken chick = new Chicken();
+			chick.putSelfInGrid(getGrid(), getLocation());
+		}
+		
+		else {
+			Color c = getColor();
+			int red = (int)(c.getRed() * (1 - DARKENING_FACTOR));
+			int green = (int)(c.getGreen() * (1 - DARKENING_FACTOR));
+			int blue = (int)(c.getBlue() * (1 - DARKENING_FACTOR));
+	
+			setColor(new Color(red, green, blue));
+		}
+	}
 }
