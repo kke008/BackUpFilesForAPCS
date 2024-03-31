1f03de6fe55d8e290e711f83040520741557c776
kke008 <130429793+kke008@users.noreply.github.com>
2024-03-30 23:00:56 -0700

    Create Fox.java

---
+++ Fox.java
@@ -0,0 +1,12 @@
+/** Create a Fox class that extends the Critter class. Set its color to be null. The Fox
+always moves to an open neighboring location at random and faces the direction it
+has moved. If it cannot move, then it turns in a random direction.
+*/
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Critter;
+import java.awt.Color;
+
+public class Fox extends Critter {
+}
