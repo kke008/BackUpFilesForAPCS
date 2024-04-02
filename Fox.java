d6769955137adb51a11c3dc80533fbb802a2d8c5
kke008 <130429793+kke008@users.noreply.github.com>
2024-04-02 11:42:01 -0700

    Update Fox.java

--- Fox.java
+++ Fox.java
@@ -1,31 +1,17 @@
-/** Create a Fox class that extends the Critter class. Set its color to be null. The Fox
-always moves to an open neighboring location at random and faces the direction it
-has moved. If it cannot move, then it turns in a random direction.
-
-Make the Fox run after the closest Chicken. Find the closest Chicken by using the
-distance formula then move to the next empty location in that
-direction. If there are no Chickens on the grid, then make a move like 1 above. If several
-Chickens are equidistant, then pick one of those directions at random. If there is no open location
-in the direction of the closest Chicken, then move to an open neighboring location at random.
-
-3. Make the Fox eat Chickens. The Chicken must be in a neighboring location to the Fox. If more
-than one Chicken is a neighbor, then the Fox picks one Chicken at random to eat. The Fox
-“eats” by replacing the Chicken with a Tombstone. After the Chicken is eaten, the Fox’s
-stomach is full and it takes a “nap” in which it does not move or eat for 10 steps. After the nap, the
-Fox is hungry again and chases after Chickens like in 2 above.
-
-Make the Fox die. A Fox dies when it has been hungry for too long. A new Fox always starts as
-though it just finished napping and is hungry. Keep track of how many steps the Fox takes while
-hungry (after nap). After 20 steps of being hungry, the Fox has now starved to death and is
-replaced in the same location with a Tombstone.
-*/
+/** Extends Critter. Foxes try to chase after and eat Chickens. If it eats,
+ *  they take a nap during which they don't move. If they do not eat, they
+ *  slowly become hungry until they die of starvation or eat. If there is
+ *  no nearest Chicken for the Fox to chase after, it moves randomly or
+ *  turns randomly.
+ * @author Karen Ke
+ * @since March 28, 2024
+ */
 import info.gridworld.grid.Grid;
 import info.gridworld.grid.Location;
 import info.gridworld.actor.Actor;
 import info.gridworld.actor.Critter;
 import java.awt.Color;
-
-COMMENTS
+import java.util.ArrayList;
 
 public class Fox extends Critter {
 	private Location locOfNearestChicken;
@@ -34,115 +20,119 @@ public class Fox extends Critter {
 	private int nap;
 	private int hungry;
   
+	/** Foxes are null in color*/
 	public Fox() {
 		setColor(null);
-		locOfNearestChicken = new Location();
+		locOfNearestChicken = getLocation();
 		nearestChickenExists = false;
 		move = true;
 		nap = 0;
 		hungry = 0;
 	}
-
+	
+	/** Overrides getActors() in Critter. Returns ArrayList of all the
+	 *  actors in the grid.
+	 *  @return actors	list of all actors in grid
+	 */
+	public ArrayList<Actor> getActors() {
+		ArrayList<Actor> actors = new ArrayList<Actor>();
+		
+		for (int r = 0; r < getGrid().getNumRows(); r++) {
+			for (int c = 0; c < getGrid().getNumCols(); c++) {
+				Actor a = getGrid().get(new Location(r, c));
+				if (a != null)
+					actors.add(a);
+			}
+		}
+		return actors;
+	}
+	
+	/** Overrides processActors() in Critter. Makes an array of nearby
+	 *  Chickens, uses the array to find the nearest Chicken (if it exists),
+	 *  and sets the location of that Chicken. If there are multiple
+	 *  "nearest" Chickens, a random one is chosen.
+	 *  @param actors		list of all nearby actors; used to make list
+	 * 						of nearby Chickens
+	 */
 	public void processActors(ArrayList<Actor> actors) {
-		ArrayList<Chicken> chickens = new ArrayList<Chicken>();
+		ArrayList<Actor> chickens = new ArrayList<Actor>();
 		for (Actor a : actors) {
-			if (a instanceOf Chicken)
+			if (a instanceof Chicken)
 				chickens.add(a);
 		}
-	
-		if (chickens.size() != 0) {
+		
+		if (chickens.size() != 0) {		// if there are chickens nearby
 			nearestChickenExists = true;
 			double longestDistance = 0;
 			for (int c = 0; c < chickens.size(); c++) {
-				Chicken chick = chickens.get(c);
+				Actor chick = chickens.get(c);
 				Location chickLoc = chick.getLocation();
-				double rowVal = Math.pow(chickLoc.getRow() - getRow(), 2);
-				double colVal = Math.pow(chickLoc.getCol() - getCol(), 2);
-				double distance = Math.squr(rowVal + colVal);
+				double diffRow = chickLoc.getRow() - getLocation().getRow();
+				double diffCol = chickLoc.getCol() - getLocation().getCol();
+				double rowVal = Math.pow(diffRow, 2);
+				double colVal = Math.pow(diffCol, 2);
+				double distance = Math.sqrt(rowVal + colVal);
 				if (distance > longestDistance) {
 					longestDistance = distance;
 					locOfNearestChicken = chick.getLocation();
 				}
 	
 				else if (distance == longestDistance) {
-					int randNum = (int)(Math.random()*2)
+					int randNum = (int)(Math.random()*2);
 						if (randNum == 1)
 						locOfNearestChicken = chick.getLocation();
 				}
 			}
+			setDirection(getLocation().getDirectionToward(locOfNearestChicken));
 		}
 	}
 	
-	public void move(ArrayList<Location> locs) {
+	/** Overrides makeMove() in Critter. If the fox has been hungry for 
+	 *  too long, it "dies". Otherwise, if the fox is not takng a nap,
+	 *  checks if nearest Chicken exists. If it does and the adjacent
+	 *  grid space in that location is valid, fox moves there and eats
+	 *  any Chicken that is there. If it eats, the hungry var is reset, 
+	 *  otherwise it is incremented. If the location is not valid, hungry
+	 *  is incremented and nearestChickenExists is reset. If the nearest
+	 *  Chicken doesn't exist and the chicken isn't napping, it moves to 
+	 *  a randomly selected location. If the Chikcen does not move, it 
+	 *  turns in a random direction. If the Chicken is napping, nap is
+	 *  decremented.
+	 * @param loc		the random location the Chicken may or may not move to
+	 */
+	public void makeMove(Location loc) {
 		if (hungry == 20) {
 			Tombstone ts = new Tombstone();
 			ts.putSelfInGrid(getGrid(), getLocation());
 		}
-		if (nap == 0) {
+		else if (nap == 0) {
 			Location loc1 = getLocation();
+			System.out.println(nearestChickenExists);	/////////////////////////////
 			if (nearestChickenExists) {
-				Direction dirToNC = loc1.getDirectionTowards(locOfNearestChicken);
-				Location locToNC = loc1.getAdjacentLocation(toNC);
+				Location locToNC = loc1.getAdjacentLocation(getDirection());
 				  
-				if (getGrid().isValid(locToNC)) {
-					Actor adjActor = getGrid().get(locToNC);
-					if (adjActor instanceOf Chicken) {
-						adjActor.removeSelfFromGrid();
+				if (locToNC.equals(locOfNearestChicken)) {
+						System.out.println("is a chicken");	//////////////////////
+						getGrid().get(locOfNearestChicken).removeSelfFromGrid();
+						nearestChickenExists = false;
 						Tombstone ts = new Tombstone();
 						ts.putSelfInGrid(getGrid(), locToNC);
 						nap = 10;
 						hungry = 0;
-					}
-					else {
-						setDirection(dirToNC);
-						moveTo(locToNC);
-						hungry++;
-					}
 				}
-				else {
-					nearestChickenExists = false;
-					hungry++;
-				}
+				moveTo(locToNC);
+				hungry++;
 			}
 		    
-			if (move && !nearestChickenExists) {
-				boolean dirWorks = false;
-				int[] randDirections = makeRandomDirections();
-				int d = -1;
-				do {
-					d++;
-					int dir = randDirections[d];
-					Location locInDir = loc1.getAdjacentLocation(dir);
-					if (getGrid().isValid(locInDir) && getGrid().get(locInDir) == null) {
-						dirWorks = true;
-						setDirection(dir);
-						moveTo(locInDir);
-					}
-				} while (d < randDirections.length && !directionWorks);
-		
-				if (!dirWorks)
-					move = false;
-			}
+			if (move && !nearestChickenExists)
+				moveTo(loc);
 		
 			if (!move) {
-				int randDir = (int)(Math.random()*7)
-				setDirection(randDir * 45);
+				setDirection((int)(Math.random()*7) * 45);
 				move = true;
 			}
 		}
 		else
 			nap--;
-	}
-	
-	public int[] makeRandomDirections() {
-		int[] randomDirections = new int[8];
-		for (int i = 0; i < 8; i++) {
-			int ind = 0;
-			while (randomDirections[ind] != 0) {
-				ind = (int)(Math.random() * 9);
-			}
-			randomDirections[ind] = i;
-		}
-		return randomDirections;
 	}
 }
